97044f1ede4b04a958f44fa0854cc5ff170f78c2
Yung Carry <c9kerdom@vasvari.org>
2024-09-10 10:39:30 +0200

    00

--- 2024_09_10/app.js
+++ 2024_09_10/app.js
@@ -1,10 +1,40 @@
-const nevek = [];
+/*const nevek = [];
 // const nevek = Array<Integer>();
 nevek.push("Peti");
 nevek.push("Laci");
 
-x = nevek + 5;
 
 console.log(nevek);
 console.log(nevek[1]);
 console.log(x);
+
+if (true) {
+    let x = 3;
+}
+var b = 3;
+
+console.log(x);
+console.log(b); */
+
+const pos = { x: 3.25, y: 5.67, name: "Budapest" };
+
+console.log(pos);
+console.log(pos.x);
+console.log(pos.name);
+
+const emberek = [
+  {
+    name: "John",
+    age: 30,
+    city: "New York",
+    children: ["Ann", "Billy"],
+  },
+  {
+    name: "Jane",
+    age: 25,
+    city: "New York",
+    children: ["Tom"],
+  },
+];
+
+console.log(emberek.city);
