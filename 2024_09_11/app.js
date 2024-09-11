fa0896b6ba277159efe5513083eac001f3255227
Yung Carry <c9kerdom@vasvari.org>
2024-09-11 15:13:14 +0200

    asd

--- 2024_09_11/app.js
+++ 2024_09_11/app.js
@@ -5,12 +5,29 @@ console.log(x === y);
 
 let szamok = null;
 
-if (szamok !== null)
+if (szamok !== null) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+/*
+let i = 5;
+
+while( i > 4)
 {
-    console.log("asd");
+    console.log("Boros budos");
 }
 
-else {
-    console.log("dsa");
-}
+*/
 
+const szamok2 = [1, 2, 3, 4, 5, 6, 7, 8];
+
+/*szamok2.forEach((e) => {
+  console.log(e);
+});
+*/
+
+//szamok2.forEach((e) => console.log(e * 2));
+
+const szuro = szamok.filter((e) => e > 3).forEach((e) => console.log(e));
